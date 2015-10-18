3c0e6c96b127ab90b390f06fafa27e006bbac5ca
eleric <eleric>
2015-10-18 12:56:21 -0400

    Debugged running from archived jar.

--- src/dr/nlp/processor/impl/ProperNounProcessor.java
+++ src/dr/nlp/processor/impl/ProperNounProcessor.java
@@ -1,5 +1,6 @@
 package dr.nlp.processor.impl;
 
+import com.sun.nio.zipfs.ZipFileSystemProvider;
 import dr.nlp.atom.Atom;
 import dr.nlp.dataInput.DataInput;
 import dr.nlp.factory.DRNLPFactory;
@@ -11,11 +12,8 @@ import dr.nlp.token.type.TokenType;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.nio.file.*;
+import java.util.*;
 
 /**
  * Created by eleri_000 on 10/17/2015.
@@ -29,14 +27,15 @@ public class ProperNounProcessor implements Processor<Character> {
 	private Atom whitSpaceAtom = factory.createWhiteSpaceAtom();
 	private Atom punctuationAtom = factory.createPunctuationAtom();
 
-	public ProperNounProcessor(Dictionary masterDictionary) throws URISyntaxException, IOException {
+	public ProperNounProcessor(Dictionary masterDictionary)
+			throws URISyntaxException, IOException {
 		this.properNouns = new ArrayList<>();
-		URL url  = this.getClass().getClassLoader().
-				getResource("resources/NER.txt");
-		Path path = Paths.get(url.toURI());
-		BufferedReader bf = new BufferedReader(new FileReader(path.toFile()));
-		for (String line = bf.readLine(); line != null; line = bf.readLine())
-		{
+		InputStream is = ClassLoader
+				.getSystemResourceAsStream("resources/NER.txt");
+		Reader r;
+		r = new InputStreamReader(is);
+		BufferedReader bf = new BufferedReader(r);
+		for (String line = bf.readLine(); line != null; line = bf.readLine()) {
 			properNouns.add(line);
 		}
 		this.masterDictionary = masterDictionary;
@@ -47,25 +46,24 @@ public class ProperNounProcessor implements Processor<Character> {
 			throws EOFException {
 		StringBuilder buf = new StringBuilder();
 		int rollbackIndex = dataInput.getIndex();
-		while (true)
-		{
+		while (true) {
 			buf.append(dataInput.get());
 			final String search = buf.toString();
 			// If search matches any proper noun then return token
-			if (properNouns.stream().anyMatch((p)->p.equals(search)))
-			{
+			if (properNouns.stream().anyMatch((p) -> p.equals(search))) {
 				char next = dataInput.get();
 				dataInput.stepBack();
 				// Proper Noun needs to be separated by white space or punctuation
 				// to be valid.
-				if (whitSpaceAtom.isValid(next)||punctuationAtom.isValid(next)) {
+				if (whitSpaceAtom.isValid(next) || punctuationAtom
+						.isValid(next)) {
 					return makeToken(search);
 				}
 			}
 			// If current search string no longer has a chance of matching a
 			// proper noun. Rollback index and return empty token.
-			if (properNouns.stream().filter((p)->p.startsWith(search)).count() == 0)
-			{
+			if (properNouns.stream().filter((p) -> p.startsWith(search)).count()
+					== 0) {
 				dataInput.setIndex(rollbackIndex);
 				return Optional.empty();
 			}
@@ -73,8 +71,7 @@ public class ProperNounProcessor implements Processor<Character> {
 		}
 	}
 
-	private Optional<Token> makeToken(String search)
-	{
+	private Optional<Token> makeToken(String search) {
 		Token token = new Token(search, TokenType.PROPER_NOUN);
 		// Only 1 token needs to exist in memory that maps to the same token
 		// value/type.
